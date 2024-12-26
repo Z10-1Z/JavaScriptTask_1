4bc14c46c94b86f0689a647089e4e9fcf5b82c02
Z10-1Z <ziadabdelaziz99@gmail.com>
2024-12-26 19:36:28 +0200

    Update JavaScript.js
    Removed comments from the code

--- JavaScript.js
+++ JavaScript.js
@@ -1,70 +1,70 @@
 // 1-Write a JavaScript program to calculate the sum of 2 number
 
-// var x = +prompt("Enter First Number")
-// var y = +prompt("Enter Second Number")
-// sum=x+y 
-// console.log("Sum = ",sum)
+var x = +prompt("Enter First Number")
+var y = +prompt("Enter Second Number")
+sum=x+y 
+console.log("Sum = ",sum)
 
 // 2-Write a JavaScript program to check number if even or odd
 
-// var checkNumber =  +prompt("Enter A Number")
-//   if (checkNumber % 2 === 0) {
-//     console.log(checkNumber + " is even.");
-//   } else {
-//     console.log(checkNumber + " is odd.");
-//   }
+ var checkNumber =  +prompt("Enter A Number")
+   if (checkNumber % 2 === 0) {
+     console.log(checkNumber + " is even.");
+   } else {
+     console.log(checkNumber + " is odd.");
+     }
 
 // 3- Write a JavaScript program that takes a month and returns the number of days in that month.
 
-// var month = prompt("Enter the name of a month:");
-// var january = 31, march = 31, may = 31, july = 31, august = 31, october = 31, december = 31;
-// var april = 30, june = 30, september = 30, november = 30;
-// var february = 28;
+ var month = prompt("Enter the name of a month:");
+ var january = 31, march = 31, may = 31, july = 31, august = 31, october = 31, december = 31;
+ var april = 30, june = 30, september = 30, november = 30;
+ var february = 28;
 
-// switch (month) {
-//     case "january":
-//     case "march":
-//     case "may":
-//     case "july":
-//     case "august":
-//     case "october":
-//     case "december":
-//         console.log("31 days");
-//         break;
-//     case "april":
-//     case "june":
-//     case "september":
-//     case "november":
-//         console.log("30 days");
-//         break;
-//     case "february":
-//         console.log("28 days");
-//         break;
-//     default:
-//         console.log("Invalid month. Please enter a valid month name.");
-// }
+ switch (month) {
+     case "january":
+     case "march":
+     case "may":
+     case "july":
+     case "august":
+     case "october":
+     case "december":
+         console.log("31 days");
+         break;
+     case "april":
+     case "june":
+     case "september":
+     case "november":
+         console.log("30 days");
+         break;
+     case "february":
+         console.log("28 days");
+         break;
+     default:
+         console.log("Invalid month. Please enter a valid month name.");
+ }
 
 // 4- Write a JavaScript program to find max number between 2 number
 
-// var num1 = +prompt("Enter the first number:");
-// var num2 = +prompt("Enter the second number:");
+ var num1 = +prompt("Enter the first number:");
+ var num2 = +prompt("Enter the second number:");
 
-// if (num1 > num2) {
-//     console.log("The maximum number is " + num1);
-// } else if (num2 > num1) {
-//     console.log("The maximum number is " + num2);
-// } else {
-//     console.log("Both numbers are equal.");
-// }
+ if (num1 > num2) {
+     console.log("The maximum number is " + num1);
+ } else if (num2 > num1) {
+     console.log("The maximum number is " + num2);
+ } else {
+     console.log("Both numbers are equal.");
+ }
 
 // 5- Write  a JavaScript program to check if character vowel or constant
 
-// var char = prompt("Enter A Single Character:");
+ var char = prompt("Enter A Single Character:");
 
-// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
-//     console.log(char + " Is A Vowel.");
-// } else if (char >= 'a' && char <= 'z') {
-//     console.log(char + " Is A Consonant.");
-// } else {
-//     console.log("Please Enter A Valid Alphabet Character.");
-// }
+ if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+     console.log(char + " Is A Vowel.");
+ } else if (char >= 'a' && char <= 'z') {
+     console.log(char + " Is A Consonant.");
+ } else {
+     console.log("Please Enter A Valid Alphabet Character.");
+ }
